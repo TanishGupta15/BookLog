a2862fe4615cf9c11af693981f70aaecaa7f3edd
Tanish Gupta <cs1200397@iitd.ac.in>
2022-07-02 19:05:21 +0530

    Resolve quote api

--- web-backend/controllers/homepage.js
+++ web-backend/controllers/homepage.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const { route } = require('./user');
+const axios =  require('axios');
 const router = express.Router();
 
-const BASE_URL = 'https://zenquotes.io/api/today'
-const key = process.env.qoutesKey
+const BASE_URL = 'https://zenquotes.io/api/today';
+const key = process.env.qoutesKey;
 
-// const api = async () => fetch({
-//   method: "POST",
-//   url: BASE_URL,
-//   headers: {
-//     key: process.env.qoutesKey
-//   }
-// })
-const getQuote = async () => {
-  const response = await fetch(`${BASE_URL} `, { method: 'GET', headers: { 'Content-Type': 'application/json' } })
-  const data = await response.json()
-  return data
-}
-
-// const asyncApiCall = async () => {
-//   const response = await QuoteAPI.getQuote()
-//   console.log(response.data)
-//   console.log(response.data[0].h)
-// }
-
-router.get('/quote', getQuote)
+router.get('/quote', async (req, res) => {
+  const data = await axios.get(`${BASE_URL}`);
+  res.send(200);
+  return data.data[0];
+});
 
 module.exports = router;
