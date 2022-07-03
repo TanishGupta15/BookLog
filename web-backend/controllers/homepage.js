710cd1a5794fc6449f103b89149575b1ce317f54
Kulvir <kulvir@Kulvirs-MB.local>
2022-07-03 19:30:28 +0530

    minor changes

--- web-backend/controllers/homepage.js
+++ web-backend/controllers/homepage.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { route } = require('./user');
-const axios =  require('axios');
+const axios = require('axios');
 const router = express.Router();
 
 const BASE_URL = 'https://zenquotes.io/api/today';
@@ -8,8 +7,8 @@ const key = process.env.qoutesKey;
 
 router.get('/quote', async (req, res) => {
   const data = await axios.get(`${BASE_URL}`);
-  res.send(200);
-  return data.data[0];
+  res.status(200).send(data.data);
+  // return data.data[0];
 });
 
 module.exports = router;
